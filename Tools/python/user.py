c15f3fdf9c36bd3a507c5b40fc299f9703d1aab2
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-03-19 11:53:05 +0100

    update file and cache locations

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -47,12 +47,14 @@ if os.environ["USER"] in ["lukas.lechner"]: #CBE cluster
     gridpack_directory                  = "/mnt/hephy/cms/lukas.lechner/TTGammaEFT/gridpacks/"
 
     analysis_results                    = results_directory
-    cache_directory                     = "/mnt/hephy/cms/lukas.lechner/TTGammaEFT/cache/"
+#    cache_directory                     = "/mnt/hephy/cms/lukas.lechner/TTGammaEFT/cache/"
+    cache_directory                     = "/users/lukas.lechner/public/cache/"
     combineReleaseLocation              = "/users/lukas.lechner/public/CMSSW_10_2_18/src/tmp/"
     cardfileLocation                    = "/mnt/hephy/cms/lukas.lechner/TTGammaEFT/results/cardfiles/"
 
     dpm_directory                       = "/mnt/hephy/cms/lukas.lechner/TTGammaEFT/nanoTuples/"
 #    dpm_directory                       = "/scratch-cbe/users/hephy/lukas.lechner/TTGammaEFT/nanoTuples/"
+#    dpm_directory                       = "/scratch/lukas.lechner/TTGammaEFT/nanoTuples/"
     eos_directory                       = "/mnt/hephy/cms/lukas.lechner/TTGammaEFT/"
 
 if os.environ["USER"] in ["ttschida"]:
