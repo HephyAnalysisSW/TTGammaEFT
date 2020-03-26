b2415dbf609d6243ef944d6dd0f8b6e791cba7f2
rosmarie schoefbeck <rosmarie.x@me.com>
2020-03-26 18:15:59 +0100

    recoPlotsEFTweight

--- plots/plotsRosi/analysisPlots/analysisPlotsEFTweight.py
+++ plots/plotsRosi/analysisPlots/analysisPlotsEFTweight.py
@@ -257,7 +257,8 @@ def sequenceExample( event, sample ):
 
 #ptweight
 def pt_weight( event, sample ):
-    if sample.name == 'ttgamma':
+    if sample.name != 'ttgamma': return
+    else :     
         if event.PhotonGood0_pt >= 400: binNumber = 20
         else: binNumber = sample.params["histo"].FindBin( event.PhotonGood0_pt )
     eftweight = sample.params["histo"].GetBinContent( binNumber )
