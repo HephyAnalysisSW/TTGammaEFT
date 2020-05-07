fa3163a534881a40a737d2abba91ca6137250ee5
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-05-07 11:44:33 +0200

    change to new pp version

--- Samples/python/default_locations.py
+++ Samples/python/default_locations.py
@@ -9,17 +9,17 @@ class postprocessing_locations:
     Run2017     = "TTGammaEFT_PP_2017_TTG_Data_v18/dilep/"
     Run2018     = "TTGammaEFT_PP_2018_TTG_Data_v18/dilep/"
 
-    MC2016_semilep  = "TTGammaEFT_PP_2016_TTG_private_v35/semilep/"
-    MC2017_semilep  = "TTGammaEFT_PP_2017_TTG_private_v35/semilep/"
-    MC2018_semilep  = "TTGammaEFT_PP_2018_TTG_private_v37/semilep/"
+    MC2016_semilep  = "TTGammaEFT_PP_2016_TTG_private_v36/semilep/"
+    MC2017_semilep  = "TTGammaEFT_PP_2017_TTG_private_v36/semilep/"
+    MC2018_semilep  = "TTGammaEFT_PP_2018_TTG_private_v38/semilep/"
 
     Run2016_semilep = "TTGammaEFT_PP_2016_TTG_Data_v36/semilep/"
     Run2017_semilep = "TTGammaEFT_PP_2017_TTG_Data_v36/semilep/"
-    Run2018_semilep = "TTGammaEFT_PP_2018_TTG_Data_v37/semilep/"
+    Run2018_semilep = "TTGammaEFT_PP_2018_TTG_Data_v38/semilep/"
 
-    MC2016_incl  = "TTGammaEFT_PP_2016_TTG_private_v35/incl/"
-    MC2017_incl  = "TTGammaEFT_PP_2017_TTG_private_v35/incl/"
-    MC2018_incl  = "TTGammaEFT_PP_2018_TTG_private_v35/incl/"
+    MC2016_incl  = "TTGammaEFT_PP_2016_TTG_private_v36/incl/"
+    MC2017_incl  = "TTGammaEFT_PP_2017_TTG_private_v36/incl/"
+    MC2018_incl  = "TTGammaEFT_PP_2018_TTG_private_v36/incl/"
 
     EFT             = "TTGammaEFT_PP_EFT_TTG_v3/gen/"
     MCSync_semilep  = "TTGammaEFT_PP_Sync_TTG_private_v39/incl/"
