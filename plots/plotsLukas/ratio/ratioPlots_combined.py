d41f4d752d9b2c26d64641a611355b01bb489949
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-05-07 11:57:51 +0200

    cleanup in fake mc ratio plots

--- plots/plotsLukas/ratio/ratioPlots_combined.py
+++ plots/plotsLukas/ratio/ratioPlots_combined.py
@@ -18,6 +18,7 @@ from TTGammaEFT.Tools.cutInterpreter  import cutInterpreter, isoThresh, chgIsoTh
 from TTGammaEFT.Tools.TriggerSelector import TriggerSelector
 from TTGammaEFT.Tools.Variables       import NanoVariables
 from TTGammaEFT.Tools.objectSelection import isBJet, photonSelector, vidNestedWPBitMapNamingListPhoton
+from TTGammaEFT.Analysis.SetupHelpers  import *
 
 from Analysis.Tools.metFilters        import getFilterCut
 from Analysis.Tools.helpers           import getCollection, deltaR
@@ -32,7 +33,6 @@ import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('--logLevel',           action='store',      default='INFO', nargs='?', choices=loggerChoices,                     help="Log level for logging")
 argParser.add_argument('--plot_directory',     action='store',      default='102X_TTG_ppv21_v1')
-#argParser.add_argument('--plotFile',           action='store',      default='all_noPhoton')
 argParser.add_argument('--selection',          action='store',      default='dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40')
 argParser.add_argument('--small',              action='store_true',                                                                       help='Run only on a small subset of the data?', )
 argParser.add_argument('--noData',             action='store_true', default=False,                                                        help='also plot data?')
@@ -41,8 +41,6 @@ argParser.add_argument('--onlyTT',             action='store_true', default=Fals
 argParser.add_argument('--onlyTTLep',          action='store_true', default=False,                                                        help="Plot only tt 2l sample")
 argParser.add_argument('--onlyTTSemiLep',      action='store_true', default=False,                                                        help="Plot only tt 1l sample")
 argParser.add_argument('--normalize',          action='store_true', default=False,                                                        help="normalize shapes")
-#argParser.add_argument('--normalize',          action='store_true', default=False,                                                        help="Normalize yields" )
-argParser.add_argument('--addOtherBg',         action='store_true', default=False,                                                        help="add others background" )
 argParser.add_argument('--categoryPhoton',     action='store',      default="PhotonNoChgIsoNoSieie0", type=str, choices=photonCatChoices,                   help="plot in terms of photon category, choose which photon to categorize!" )
 argParser.add_argument('--mode',               action='store',      default="None",    type=str, choices=["mu", "e", "mumu", "mue", "ee", "SF", "all", "SFtight", "eetight", "mumutight", "muetight"], help="plot lepton mode" )
 argParser.add_argument('--nJobs',              action='store',      default=1,         type=int, choices=[1,2,3,4,5],                        help="Maximum number of simultaneous jobs.")
@@ -61,37 +59,21 @@ if args.noData:          args.plot_directory += "_noData"
 if args.normalize:       args.plot_directory += "_normalize"
 
 # Samples
-os.environ["gammaSkim"]="True"# if ("hoton" in args.selection or "pTG" in args.selection) and not args.invLeptonIso else "False"
-if "dilep" in args.selection:
-    if args.year == 2016:
-        from TTGammaEFT.Samples.nanoTuples_Summer16_private_postProcessed      import *
-        if not args.noData:
-            from TTGammaEFT.Samples.nanoTuples_Run2016_14Dec2018_postProcessed import *
+os.environ["gammaSkim"]="True"
+if args.year == 2016:
+    from TTGammaEFT.Samples.nanoTuples_Summer16_private_semilep_postProcessed      import *
+    if not args.noData:
+        from TTGammaEFT.Samples.nanoTuples_Run2016_14Dec2018_semilep_postProcessed import *
 
-    elif args.year == 2017:
-        from TTGammaEFT.Samples.nanoTuples_Fall17_private_postProcessed        import *
-        if not args.noData:
-            from TTGammaEFT.Samples.nanoTuples_Run2017_14Dec2018_postProcessed import *
+elif args.year == 2017:
+    from TTGammaEFT.Samples.nanoTuples_Fall17_private_semilep_postProcessed        import *
+    if not args.noData:
+        from TTGammaEFT.Samples.nanoTuples_Run2017_14Dec2018_semilep_postProcessed import *
 
-    elif args.year == 2018:
-        from TTGammaEFT.Samples.nanoTuples_Autumn18_private_postProcessed      import *
-        if not args.noData:
-            from TTGammaEFT.Samples.nanoTuples_Run2018_14Dec2018_postProcessed import *
-else:
-    if args.year == 2016:
-        from TTGammaEFT.Samples.nanoTuples_Summer16_private_semilep_postProcessed      import *
-        if not args.noData:
-            from TTGammaEFT.Samples.nanoTuples_Run2016_14Dec2018_semilep_postProcessed import *
-
-    elif args.year == 2017:
-        from TTGammaEFT.Samples.nanoTuples_Fall17_private_semilep_postProcessed        import *
-        if not args.noData:
-            from TTGammaEFT.Samples.nanoTuples_Run2017_14Dec2018_semilep_postProcessed import *
-
-    elif args.year == 2018:
-        from TTGammaEFT.Samples.nanoTuples_Autumn18_private_semilep_postProcessed      import *
-        if not args.noData:
-            from TTGammaEFT.Samples.nanoTuples_Run2018_14Dec2018_semilep_postProcessed import *
+elif args.year == 2018:
+    from TTGammaEFT.Samples.nanoTuples_Autumn18_private_semilep_postProcessed      import *
+    if not args.noData:
+        from TTGammaEFT.Samples.nanoTuples_Run2018_14Dec2018_semilep_postProcessed import *
 
 
 # Text on the plots
@@ -114,7 +96,7 @@ def drawPlots( plots, mode ):
         if args.onlyTTSemiLep: sc += "1l_"
         sc += args.sideband + "_"
         sc += "log" if log else "lin"
-        plot_directory_ = os.path.join( plot_directory, 'ratioPlots_classic', str(args.year), args.plot_directory, args.selection, mode, sc )
+        plot_directory_ = os.path.join( plot_directory, 'ratioPlots', str(args.year), args.plot_directory, args.selection, mode, sc )
 
         for plot in plots:
             if not max(l[0].GetMaximum() for l in plot.histos): 
@@ -177,7 +159,7 @@ read_variables  = ["weight/F",
 #                   "fixedGridRhoFastjetAll/F",
 #                   "fixedGridRhoFastjetCentralChargedPileUp/F",
                    "PV_npvsGood/I",
-                   "Jet[%s]" %jetVarString,
+#                   "Jet[%s]" %jetVarString,
                    "PV_npvs/I", "PV_npvsGood/I",
                    "nJetGood/I", "nBTagGood/I",
                    "nJet/I", "nBTag/I",
@@ -202,17 +184,11 @@ read_variables  = ["weight/F",
                    "l1GammadR/F", "l1GammadPhi/F",
                    "j0GammadR/F", "j0GammadPhi/F",
                    "j1GammadR/F", "j1GammadPhi/F",
+                   "reweightHEM/F",
                   ]
 
 read_variables += [ "%s_photonCat/I"%item for item in photonCatChoices if item != "None" ]
 read_variables += [ "%s_photonCatMagic/I"%item for item in photonCatChoices if item != "None" ]
-
-#read_variables += [ VectorTreeVariable.fromString('Lepton[%s]'%leptonVarString, nMax=10) ]
-#read_variables += [ VectorTreeVariable.fromString('Photon[%s]'%photonVarString, nMax=10) ]
-#read_variables += [ VectorTreeVariable.fromString('Photon[%s]'%photonVarString, nMax=10) ]
-#read_variables += [ VectorTreeVariable.fromString('Jet[%s]'%jetVarString, nMax=10) ]
-#read_variables += [ VectorTreeVariable.fromString('JetGood[%s]'%jetVarString, nMax=10) ]
-
 
 read_variables_MC = ["isTTGamma/I", "isZWGamma/I", "isTGamma/I", "overlapRemoval/I",
                      "reweightPU/F", "reweightPUDown/F", "reweightPUUp/F", "reweightPUVDown/F", "reweightPUVUp/F",
@@ -221,7 +197,7 @@ read_variables_MC = ["isTTGamma/I", "isZWGamma/I", "isTGamma/I", "overlapRemoval
                      "reweightLeptonMediumSF/F", "reweightLeptonMediumSFUp/F", "reweightLeptonMediumSFDown/F",
                      "reweightLeptonTracking2lSF/F",
                      "reweightTrigger/F", "reweightTriggerUp/F", "reweightTriggerDown/F",
-                     "reweightInvTrigger/F", "reweightInvTriggerUp/F", "reweightInvTriggerDown/F",
+                     "reweightInvIsoTrigger/F", "reweightInvIsoTriggerUp/F", "reweightInvIsoTriggerDown/F",
                      "reweightPhotonSF/F", "reweightPhotonSFUp/F", "reweightPhotonSFDown/F",
                      "reweightPhotonElectronVetoSF/F",
                      "reweightBTag_SF/F", "reweightBTag_SF_b_Down/F", "reweightBTag_SF_b_Up/F", "reweightBTag_SF_l_Down/F", "reweightBTag_SF_l_Up/F",
@@ -234,9 +210,7 @@ photonVarList   += ["genPartIdx"]
 read_variables_MC += map( lambda var: "PhotonGood0_"             + var, photonVariables )
 read_variables    += map( lambda var: "JetGood0_"                + var, jetVariables )
 read_variables    += map( lambda var: args.categoryPhoton + "_"  + var, photonVariables )
-read_variables_MC += [ VectorTreeVariable.fromString('GenPart[%s]'%genVarString, nMax=1000) ]
-#read_variables_MC += [ "%s_genPartIdx/I"%item for item in photonCatChoices if item != "None" ]
-#read_variables_MC += [ "PhotonGood0_genPartIdx/I" ]
+#read_variables_MC += [ VectorTreeVariable.fromString('GenPart[%s]'%genVarString, nMax=1000) ]
 recoPhotonSel_medium    = photonSelector( 'medium', year=args.year )
 
 def getPhotons( event, sample ):
@@ -269,35 +243,26 @@ def minDR( event, sample ):
 
     event.photonJetdR = min( deltaR( reco, j ) if deltaR( reco, j ) > 0.0005 else 999 for j in allJets )    if allJets    else -1
     event.photonLepdR = min( deltaR( reco, j ) if deltaR( reco, j ) > 0.0005 else 999 for j in allLeptons ) if allLeptons else -1
-#    print event.photonJetdR
-#    print event.photonLepdR
 
 # Sequence
 sequence = []
-#sequence = [ checkParentList ]# makePhotons ]#\
-#            clean_Jets,
-#            make_Zpt,
-#           ]
 
 # Sample definition
 if args.year == 2016:
     if args.onlyTT: all = TT_pow_16
     elif args.onlyTTLep: all = TT_Lep_16
     elif args.onlyTTSemiLep: all = TT_SemiLep_16
-    elif args.addOtherBg: all = all_16
-    else: all = all_noOther_16
+    else: all = all_16
 elif args.year == 2017:
     if args.onlyTT: all = TT_pow_17
     elif args.onlyTTLep: all = TT_Lep_17
     elif args.onlyTTSemiLep: all = TT_SemiLep_17
-    elif args.addOtherBg: all = all_17
-    else: all = all_noOther_17
+    else: all = all_17
 elif args.year == 2018:
     if args.onlyTT: all = TT_pow_18
     elif args.onlyTTLep: all = TT_Lep_18
     elif args.onlyTTSemiLep: all = TT_SemiLep_18
-    elif args.addOtherBg: all = all_18
-    else: all = all_noOther_18
+    else: all = all_18
 
 all_sb = copy.deepcopy(all)
 all_sb.name = "sb"
@@ -343,14 +308,26 @@ else:
 
 stack = Stack( *stackSamples )
 
+if "Jet2" in args.selection and not "Jet2p" in args.selection:
+    misIDSF_val = misID2SF_val
+elif "Jet3" in args.selection and not "Jet3p" in args.selection:
+    misIDSF_val = misID3SF_val
+elif "Jet4" in args.selection and not "Jet4p" in args.selection:
+    misIDSF_val = misID4SF_val
+elif "Jet5" in args.selection:
+    misIDSF_val = misID5SF_val
+elif "Jet2p" in args.selection:
+    misIDSF_val = misID2pSF_val
+elif "Jet3p" in args.selection:
+    misIDSF_val = misID3pSF_val
+elif "Jet4p" in args.selection:
+    misIDSF_val = misID4pSF_val
+
 for sample in mc:
     sample.read_variables = read_variables_MC
     sample.scale          = lumi_scale
     sample.style          = styles.fillStyle( sample.color )
-    if "dilep" in args.selection:
-        sample.weight         = lambda event, sample: event.reweightL1Prefire*event.reweightPU*event.reweightLeptonMediumSF*event.reweightLeptonTracking2lSF*event.reweightPhotonSF*event.reweightPhotonElectronVetoSF*event.reweightBTag_SF
-    else:
-        sample.weight         = lambda event, sample: event.reweightTrigger*event.reweightL1Prefire*event.reweightPU*event.reweightLeptonTightSF*event.reweightLeptonTrackingTightSF*event.reweightPhotonSF*event.reweightPhotonElectronVetoSF*event.reweightBTag_SF
+    sample.weight         = lambda event, sample: (event.reweightHEM*event.reweightTrigger*event.reweightL1Prefire*event.reweightPU*event.reweightLeptonTightSF*event.reweightLeptonTrackingTightSF*event.reweightPhotonSF*event.reweightPhotonElectronVetoSF*event.reweightBTag_SF)+((event.nPhotonNoChgIsoNoSieie>0)*(event.PhotonNoChgIsoNoSieie0_photonCatMagic==2)*(misIDSF_val[args.year].val-1)*event.reweightHEM*event.reweightTrigger*event.reweightL1Prefire*event.reweightPU*event.reweightLeptonTightSF*event.reweightLeptonTrackingTightSF*event.reweightPhotonSF*event.reweightPhotonElectronVetoSF*event.reweightBTag_SF)
 
 if args.small:
     for sample in stack.samples:
@@ -361,19 +338,8 @@ if args.small:
 weight_ = lambda event, sample: event.weight
 
 # Use some defaults (set defaults before you create/import list of Plots!!)
-#preSelection = "&&".join( [ cutInterpreter.cutString( args.selection ), "overlapRemoval==1"] )
 preSelection = "&&".join( [ cutInterpreter.cutString( args.selection ) ] )
 Plot.setDefaults( stack=stack, weight=staticmethod( weight_ ), selectionString=preSelection )#, addOverFlowBin='upper' )
-
-# Import plots list (AFTER setDefaults!!)
-#plotListFile = os.path.join( os.path.dirname( os.path.realpath( __file__ ) ), 'plotLists', args.plotFile + '.py' )
-#if not os.path.isfile( plotListFile ):
-#    logger.info( "Plot file not found: %s", plotListFile )
-#    sys.exit(1)
-
-#plotModule = imp.load_source( "plotLists", os.path.expandvars( plotListFile ) )
-#if args.noData: from plotLists import plotListDataMC as plotList
-#else:           from plotLists import plotListData   as plotList
 
 # plotList
 addPlots = []
@@ -940,15 +906,13 @@ allPlots = {}
 if args.mode != "None":
     allModes = [ args.mode ]
 elif args.nJobs != 1:
-    allModes = [ 'mumu', 'mue', 'ee', 'SF', 'all'] if "nLepTight2" in args.selection or "dilep" in args.selection else [ "mu", "e", "all" ]
+    allModes = [ "mu", "e", "all" ]
     allModes = splitList( allModes, args.nJobs)[args.job]
 else:
-    allModes = [ 'mumu', 'mue', 'ee' ] if "nLepTight2" in args.selection or "dilep" in args.selection else [ "mu", "e" ]
+    allModes = [ "mu", "e" ]
 
 filterCutData = getFilterCut( args.year, isData=True, skipBadChargedCandidate=True )
 filterCutMc   = getFilterCut( args.year, isData=False, skipBadChargedCandidate=True )
-tr            = TriggerSelector( args.year, singleLepton="nLepTight1" in args.selection )
-triggerCutMc  = tr.getSelection( "MC" )
 
 if args.sideband == "sieie":
     sb_sel  = ["%s_sieie>%f"%(args.categoryPhoton, highSieieThresh) ] #, "%s_sieie<0.02"%(args.categoryPhoton) ]
@@ -967,19 +931,14 @@ for index, mode in enumerate( allModes ):
 
     # always initialize with [], elso you get in trouble with pythons references!
     plots  = []
-#    plots += plotList
-#    plots += [ getYieldPlot( index ) ]
     plots += addPlots
 
     # Define 2l selections
     leptonSelection = cutInterpreter.cutString( mode )
-#    mcSelection = [ filterCutMc, leptonSelection, triggerCutMc ] if args.onlyTT else mcSelection = [ filterCutMc, leptonSelection, triggerCutMc, "overlapRemoval==1" ]
     mcSelection = [ filterCutMc, leptonSelection, "triggered==1", "overlapRemoval==1" ]
-#    if not args.noData:    data_sample.setSelectionString( [ filterCutData, leptonSelection ] )
-#    for sample in mc: sample.setSelectionString( mcSelection )
 
     # sideband/fit region cuts
-    if not args.noData: data_sample.setSelectionString( [filterCutData, "triggered==1", leptonSelection ] + sb_sel )
+    if not args.noData: data_sample.setSelectionString( [filterCutData, "triggered==1", leptonSelection, "reweightHEM>0" ] + sb_sel )
     all_sb.setSelectionString( mcSelection + sb_sel )
     all_fit.setSelectionString( mcSelection + fit_sel )
 
@@ -993,21 +952,11 @@ if args.mode != "None" or args.nJobs != 1:
     sys.exit(0)
 
 # Add the different channels into SF and all
-if "nLepTight2" in args.selection or "dilep" in args.selection:
-    for mode in [ "SF", "all" ]:
-        for plot in allPlots['mumu']:
-            for pl in ( p for p in ( allPlots['ee'] if mode=="SF" else allPlots["mue"] ) if p.name == plot.name ):  #For SF add EE, second round add EMu for all
-                for i, j in enumerate( list( itertools.chain.from_iterable( plot.histos ) ) ):
-                    j.Add( list( itertools.chain.from_iterable( pl.histos ) )[i] )
+for plot in allPlots['mu']:
+    for pl in ( p for p in allPlots['e'] if p.name == plot.name ):
+        for i, j in enumerate( list( itertools.chain.from_iterable( plot.histos ) ) ):
+            j.Add( list( itertools.chain.from_iterable( pl.histos ) )[i] )
 
-        drawPlots( allPlots['mumu'], mode )
-
-else:
-    for plot in allPlots['mu']:
-        for pl in ( p for p in allPlots['e'] if p.name == plot.name ):
-            for i, j in enumerate( list( itertools.chain.from_iterable( plot.histos ) ) ):
-                j.Add( list( itertools.chain.from_iterable( pl.histos ) )[i] )
-
-    drawPlots( allPlots['mu'], "all" )
+drawPlots( allPlots['mu'], "all" )
 
 
---
+++ plots/plotsLukas/ratio/ratioPlots_combined.sh
@@ -0,0 +1,7 @@
+python ratioPlots_combined.py --noData --normalize --year 2016 --selection nLepTight1-nLepVeto1-nNoChgIsoNoSieieJet2-nNoChgIsoNoSieieBTag1p-nHadPhoton1-noChgIsoNoSieiephotoncat134 --plot_directory 102X_TTG_ppv36_v2 --sideband chgIso #SPLIT3
+python ratioPlots_combined.py --noData --normalize --year 2016 --selection nLepTight1-nLepVeto1-nNoChgIsoNoSieieJet3-nNoChgIsoNoSieieBTag1p-nHadPhoton1-noChgIsoNoSieiephotoncat134 --plot_directory 102X_TTG_ppv36_v2 --sideband chgIso #SPLIT3
+python ratioPlots_combined.py --noData --normalize --year 2016 --selection nLepTight1-nLepVeto1-nNoChgIsoNoSieieJet4p-nNoChgIsoNoSieieBTag1p-nHadPhoton1-noChgIsoNoSieiephotoncat134 --plot_directory 102X_TTG_ppv36_v2 --sideband chgIso #SPLIT3
+
+python ratioPlots_combined.py --noData --normalize --year 2016 --selection nLepTight1-nLepVeto1-nNoChgIsoNoSieieJet2-nNoChgIsoNoSieieBTag1p-nHadPhoton1-noChgIsoNoSieiephotoncat134 --plot_directory 102X_TTG_ppv36_v2 --sideband sieie #SPLIT3
+python ratioPlots_combined.py --noData --normalize --year 2016 --selection nLepTight1-nLepVeto1-nNoChgIsoNoSieieJet3-nNoChgIsoNoSieieBTag1p-nHadPhoton1-noChgIsoNoSieiephotoncat134 --plot_directory 102X_TTG_ppv36_v2 --sideband sieie #SPLIT3
+python ratioPlots_combined.py --noData --normalize --year 2016 --selection nLepTight1-nLepVeto1-nNoChgIsoNoSieieJet4p-nNoChgIsoNoSieieBTag1p-nHadPhoton1-noChgIsoNoSieiephotoncat134 --plot_directory 102X_TTG_ppv36_v2 --sideband sieie #SPLIT3
