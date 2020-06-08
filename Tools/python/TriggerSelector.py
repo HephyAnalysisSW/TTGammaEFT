2a44d973cef7c01dc5bd24d5b33d946ba59192d1
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-06-08 08:29:59 +0200

    update 2017 singleE trigger

--- Tools/python/TriggerSelector.py
+++ Tools/python/TriggerSelector.py
@@ -7,36 +7,34 @@ class TriggerSelector:
                 self.m  = [ "HLT_IsoMu24", "HLT_IsoTkMu24" ]
                 self.e  = [ "HLT_Ele27_WPTight_Gsf" ]
         elif year == 2017:
-                self.m  = [ "HLT_IsoMu24" ]
-                self.e  = [ "HLT_Ele32_WPTight_Gsf", "HLT_Ele32_WPTight_Gsf_L1DoubleEG" ]
+                self.m   = [ "HLT_IsoMu24" ]
+                self.e   = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG" ]
+#                self.e   = [ "HLT_Ele32_WPTight_Gsf", "HLT_Ele32_WPTight_Gsf_L1DoubleEG" ]
+                self.eL1 = [ "L1_SingleEG24", "L1_SingleEG26", "L1_SingleEG30", "L1_SingleEG32", "L1_SingleEG34", "L1_SingleEG36", "L1_SingleEG38", "L1_SingleEG40", "L1_SingleEG42", "L1_SingleEG45", "L1_SingleEG50", "L1_SingleEG34er2p1", "L1_SingleEG36er2p1", "L1_SingleEG38er2p1", "L1_SingleIsoEG24er2p1", "L1_SingleIsoEG26er2p1", "L1_SingleIsoEG28er2p1", "L1_SingleIsoEG30er2p1", "L1_SingleIsoEG32er2p1", "L1_SingleIsoEG34er2p1", "L1_SingleIsoEG36er2p1", "L1_SingleIsoEG24", "L1_SingleIsoEG26", "L1_SingleIsoEG28", "L1_SingleIsoEG30", "L1_SingleIsoEG32", "L1_SingleIsoEG34", "L1_SingleIsoEG36", "L1_SingleIsoEG38"]
         elif year == 2018:
                 self.m  = [ "HLT_IsoMu24" ]
                 self.e  = [ "HLT_Ele32_WPTight_Gsf" ]
         else:
             raise NotImplementedError( "Trigger selection %r not implemented" %year )
 
-        # define an arbitrary hierarchy
-        self.PDHierarchy = [ "SingleMuon", "EGamma" if year == 2018 else "SingleElectron" ]
-
-        # define which triggers should be used for which dataset
-        self.SingleMuon     = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.m]  )
-        self.SingleElectron = "(%s)"%"||".join( ["Alt$(%s,0)"%trigger for trigger in self.e]  )
-        self.EGamma         = self.SingleElectron
-
-    def __getVeto( self, cutString ):
-        return "!%s" %cutString
+        self.year = year
+        self.SingleMuon     = "("+"||".join( self.m  )+")"
+        self.SingleElectron = "("+"||".join( self.e  )+")"
+        if self.year == 2017:
+            self.SingleElectron += "&&("+"||".join( ["Alt$(%s,0)"%trigger for trigger in self.eL1] )+")"
+        elif self.year == 2018:
+            self.EGamma         = self.SingleElectron
 
     def getDataTrigger( self, PD ):
+        # define which triggers should be used for which dataset
         cutString = getattr( self, PD.split("_")[0] )
         return cutString
-#        for x in self.PDHierarchy:
-#            if x in PD: continue
-#            cutString += "&&!"
-#            cutString += getattr( self, x )
-#        return "(%s)" %cutString
 
     def getTriggerVariableList( self ):
-        return [ trigger + "/O" for trigger in self.m+self.e ]
+        if self.year == 2017:
+            return [ trigger + "/O" for trigger in self.m+self.e+self.eL1 ]
+        else:
+            return [ trigger + "/O" for trigger in self.m+self.e ]
 
     def getSelection( self ):
         return "(triggered==1)"
@@ -46,18 +44,22 @@ class TriggerSelector:
         elif abs(pdgId) == 13: triggerList = self.m
         else:                  triggerList = []
 
-        def func( event ):
-            for tr in triggerList:
-                if getattr( event, tr ): return True
-            return False
+        if self.year == 2017 and abs(pdgId) == 11:
+            # check L1 trigger for e-trigger in 2017 as recommended in TopTrigger
+            def func( event ):
+                for tr in triggerList:
+                    if getattr( event, tr ):
+                        for lTr in self.eL1:
+                            if getattr( event, lTr ): return True
+                return False
+        else:
+            def func( event ):
+                for tr in triggerList:
+                    if getattr( event, tr ): return True
+                return False
 
         return func
 
-
 if __name__=="__main__":
-    tr            = TriggerSelector( 2016, singleLepton=True )
-    triggerCutMc  = tr.getSelection( "MC" )
-    print triggerCutMc
-    Ts          = TriggerSelector( 2016, singleLepton=True )
-    triggerCond = Ts.getSelection( "SingleMuon" )
-    print triggerCond
+    tr = TriggerSelector(2017)
+    print tr.getDataTrigger( "SingleElectron" )
