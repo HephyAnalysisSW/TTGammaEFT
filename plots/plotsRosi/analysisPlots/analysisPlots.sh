12b4afb2c1f0f83f0ada45a7429ac680859fc6ba
rosmarie schoefbeck <rosmarie.x@me.com>
2020-07-17 14:17:57 +0200

    analysisPlots

---
+++ plots/plotsRosi/analysisPlots/analysisPlots.sh
@@ -0,0 +1,7 @@
+python analysisPlots.py --year 2016 --selection nLepTight1-nLepVeto1-nJet4p-nBTag1p-nPhoton1 --plot_directory myFirstPlots --mode e
+python analysisPlots.py --year 2016 --selection nLepTight1-nLepVeto1-nJet4p-nBTag1p-nPhoton1 --plot_directory myFirstPlots --mode mu
+python analysisPlots.py --year 2016 --selection nLepTight1-nLepVeto1-nJet4p-nBTag1p-nPhoton1 --plot_directory myFirstPlots --mode all
+
+python analysisPlots.py --noData --year 2016 --selection nLepTight1-nLepVeto1-nJet4p-nBTag1p-nPhoton1 --plot_directory myFirstPlots --mode e
+python analysisPlots.py --noData --year 2016 --selection nLepTight1-nLepVeto1-nJet4p-nBTag1p-nPhoton1 --plot_directory myFirstPlots --mode mu
+python analysisPlots.py --noData --year 2016 --selection nLepTight1-nLepVeto1-nJet4p-nBTag1p-nPhoton1 --plot_directory myFirstPlots --mode all
