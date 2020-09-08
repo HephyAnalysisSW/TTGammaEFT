c284bb29ffc1252c3b4a4b8f20143c3890718225
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-09-08 10:09:23 +0200

    minor

--- Analysis/python/run/cache_transferFactor.sh
+++ Analysis/python/run/cache_transferFactor.sh
@@ -1,19 +1,19 @@
-python cache_transferFactor.py --controlRegion WJets2 --mode all --overwrite --year 2016 #SPLIT240
-python cache_transferFactor.py --controlRegion TT2    --mode all --overwrite --year 2016 #SPLIT240
-python cache_transferFactor.py --controlRegion misTT2 --mode all --overwrite --year 2016 #SPLIT240
-python cache_transferFactor.py --controlRegion misTT1 --mode all --overwrite --year 2016 #SPLIT240
+python cache_transferFactor.py --controlRegion WJets2 --mode all --year 2016 #SPLIT700
+python cache_transferFactor.py --controlRegion TT2    --mode all --year 2016 #SPLIT700
+python cache_transferFactor.py --controlRegion misTT2 --mode all --year 2016 #SPLIT700
+python cache_transferFactor.py --controlRegion misTT1 --mode all --year 2016 #SPLIT700
 
-#python cache_transferFactor.py --controlRegion WJets2 --mode all --overwrite --year 2017 #SPLIT240
-#python cache_transferFactor.py --controlRegion TT2    --mode all --overwrite --year 2017 #SPLIT240
-#python cache_transferFactor.py --controlRegion misTT2 --mode all --overwrite --year 2017 #SPLIT240
-#python cache_transferFactor.py --controlRegion misTT1 --mode all --overwrite --year 2017 #SPLIT240
+python cache_transferFactor.py --controlRegion WJets2 --mode all --year 2017 #SPLIT700
+python cache_transferFactor.py --controlRegion TT2    --mode all --year 2017 #SPLIT700
+python cache_transferFactor.py --controlRegion misTT2 --mode all --year 2017 #SPLIT700
+python cache_transferFactor.py --controlRegion misTT1 --mode all --year 2017 #SPLIT700
 
-#python cache_transferFactor.py --controlRegion WJets2 --mode all --overwrite --year 2018 #SPLIT240
-#python cache_transferFactor.py --controlRegion TT2    --mode all --overwrite --year 2018 #SPLIT240
-#python cache_transferFactor.py --controlRegion misTT2 --mode all --overwrite --year 2018 #SPLIT240
-#python cache_transferFactor.py --controlRegion misTT1 --mode all --overwrite --year 2018 #SPLIT240
+python cache_transferFactor.py --controlRegion WJets2 --mode all --year 2018 #SPLIT700
+python cache_transferFactor.py --controlRegion TT2    --mode all --year 2018 #SPLIT700
+python cache_transferFactor.py --controlRegion misTT2 --mode all --year 2018 #SPLIT700
+python cache_transferFactor.py --controlRegion misTT1 --mode all --year 2018 #SPLIT700
 
-#python cache_transferFactor.py --controlRegion WJets2 --mode all --overwrite --year RunII #SPLIT240
-#python cache_transferFactor.py --controlRegion TT2    --mode all --overwrite --year RunII #SPLIT240
-#python cache_transferFactor.py --controlRegion misTT2 --mode all --overwrite --year RunII #SPLIT240
-#python cache_transferFactor.py --controlRegion misTT1 --mode all --overwrite --year RunII #SPLIT240
+#python cache_transferFactor.py --controlRegion WJets2 --mode all --year RunII #SPLIT700
+#python cache_transferFactor.py --controlRegion TT2    --mode all --year RunII #SPLIT700
+#python cache_transferFactor.py --controlRegion misTT2 --mode all --year RunII #SPLIT700
+#python cache_transferFactor.py --controlRegion misTT1 --mode all --year RunII #SPLIT700
