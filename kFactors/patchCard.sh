19575aa831cdd62f3fb43a0929a058b66d62576d
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-10-21 08:12:39 +0200

    adding scripts for kfactor est in madgraph

---
+++ kFactors/patchCard.sh
@@ -0,0 +1,3 @@
+cp cards/${1}/${1}_run_card.dat ${1}/Cards/run_card.dat
+cp cards/param_card.dat ${1}/Cards/param_card.dat
+cp cards/FKS_params.dat ${1}/Cards/FKS_params.dat
---
+++ kFactors/runXSec.sh
@@ -0,0 +1,4 @@
+launch ttGamma_Dilept_5f_ckm_NLOmue_1line_fidPS -i
+launch -x NLO
+0
+0
