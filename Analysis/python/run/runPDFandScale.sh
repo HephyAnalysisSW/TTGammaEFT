15d0179b54a99b15c3d4191ff4a62bb8b85e8109
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-12-23 09:36:24 +0100

    bugfix in pdf uncertainty

---
+++ Analysis/python/run/runPDFandScale.sh
@@ -0,0 +1,100 @@
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY3 --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY3 --selectEstimator TTG_NLO_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY4p --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY4p --selectEstimator TTG_NLO_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG3 --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG3 --selectEstimator TTG_NLO_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG4p --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG4p --selectEstimator TTG_NLO_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3M3 --selectEstimator TTG_NLO_gen #SP43
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3M3 --selectEstimator TTG_NLO_misID #SP43
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pM3 --selectEstimator TTG_NLO_gen #SP43
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pM3 --selectEstimator TTG_NLO_misID #SP43
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3PtUnfold --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3PtUnfold --selectEstimator TTG_NLO_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pPtUnfold --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pPtUnfold --selectEstimator TTG_NLO_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pPtUnfold --selectEstimator TTG_NLO_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pPtUnfold --selectEstimator TTG_NLO_misID #SP57
+
+
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3dRUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3dRUnfold --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pdRUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pdRUnfold --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pdRUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pdRUnfold --selectEstimator TTG_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3PtUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3PtUnfold --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pPtUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pPtUnfold --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pPtUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pPtUnfold --selectEstimator TTG_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3AbsEtaUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3AbsEtaUnfold --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pAbsEtaUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pAbsEtaUnfold --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pAbsEtaUnfold --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pAbsEtaUnfold --selectEstimator TTG_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY3 --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY3 --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY4p --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY4p --selectEstimator TTG_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG3 --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG3 --selectEstimator TTG_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG4p --selectEstimator TTG_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG4p --selectEstimator TTG_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3M3 --selectEstimator TTG_gen #SP43
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3M3 --selectEstimator TTG_misID #SP43
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pM3 --selectEstimator TTG_gen #SP43
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pM3 --selectEstimator TTG_misID #SP43
+
+
+
+
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3dRUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3dRUnfold --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pdRUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pdRUnfold --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pdRUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pdRUnfold --selectEstimator TT_pow_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3PtUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3PtUnfold --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pPtUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pPtUnfold --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pPtUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pPtUnfold --selectEstimator TT_pow_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3AbsEtaUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3AbsEtaUnfold --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pAbsEtaUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pAbsEtaUnfold --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pAbsEtaUnfold --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3pAbsEtaUnfold --selectEstimator TT_pow_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY3 --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY3 --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY4p --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion misDY4p --selectEstimator TT_pow_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG3 --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG3 --selectEstimator TT_pow_misID #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG4p --selectEstimator TT_pow_gen #SP57
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion VG4p --selectEstimator TT_pow_misID #SP57
+
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3M3 --selectEstimator TT_pow_gen #SP143
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR3M3 --selectEstimator TT_pow_misID #SP143
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pM3 --selectEstimator TT_pow_gen #SP143
+python runPDFandScale.py --runOnTopNanoAOD --cores 1 --notNormalized --combine --year 2016 --controlRegion SR4pM3 --selectEstimator TT_pow_misID #SP143
+
