f55678d65a7fb7605771ae6bd7db7a83a9f18692
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2021-02-08 13:41:44 +0100

    fast PP sh

---
+++ postprocessing/nanoPostProcessing_Autumn18_private_fastSim.sh
@@ -0,0 +1,9 @@
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample ttG_noFullyHad_fast #SPLIT100
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample ttZ01j_fast #SPLIT98
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample ttW01j_fast #SPLIT100
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample WZTo3L1Nu_fast #SPLIT100
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample WZTojj2L_fast #SPLIT 99
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample WZToLNujj_fast #SPLIT100
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample WGToLNu_fast #SPLIT100
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample ZGTo2L_fast #SPLIT97
+python nanoPostProcessing.py --overwrite --skim semilep --year 2018 --processingEra TTGammaEFT_PP_2018_TTG_private_v50 --fileBasedSplitting  --private --sample WW_fast #SPLIT100
