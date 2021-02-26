6c2d88fb6a96f63184df0aeb68af4301a8803343
Lukas Lechner <lukas.k.lechner@gmail.com>
2021-02-26 10:15:37 +0100

    minor

--- plots/plotsLukas/systematics/cacheAll.sh
+++ plots/plotsLukas/systematics/cacheAll.sh
@@ -1,1 +1,1 @@
-while read x; do echo $x; cmssw ${x}; submit --title sys16 missing.sh; done < systematicVariation.sh
+while read x; do echo $x; rm missing.sh; cmssw ${x}; submit --title sys16 missing.sh; done < systematicVariation.sh
