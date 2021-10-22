9ba387357883274d160fa0a42b39c41d75d08656
Lukas Lechner <lukas.lechner@cern.ch>
2021-10-22 10:22:37 +0200

    minor

---
+++ Analysis/python/run/cache_fakes.sh
@@ -0,0 +1,28 @@
+
+#python run_estimate.py --cores 1 --controlRegion SR3M3 --selectEstimator fakes-DD --year 2018 #SPLIT501
+#python run_estimate.py --cores 1 --controlRegion SR4pM3 --selectEstimator fakes-DD --year 2018 #SPLIT501
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3dRUnfold --selectEstimator fakes-DD --year 2018 #SPLIT335
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3pdRUnfold --selectEstimator fakes-DD --year 2018 #SPLIT53
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR4pdRUnfold --selectEstimator fakes-DD --year 2018 #SPLIT335
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3AbsEtaUnfold --selectEstimator fakes-DD --year 2018 #SPLIT314
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3pAbsEtaUnfold --selectEstimator fakes-DD --year 2018 #SPLIT44
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR4pAbsEtaUnfold --selectEstimator fakes-DD --year 2018 #SPLIT314
+#python run_estimate.py --cores 1 --controlRegion SR3PtUnfold --selectEstimator fakes-DD --year 2018 #SPLIT308
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3pPtUnfold --selectEstimator fakes-DD --year 2018 #SPLIT97
+#python run_estimate.py --cores 1 --controlRegion SR4pPtUnfold --selectEstimator fakes-DD --year 2018 #SPLIT308
+python run_estimate.py --cores 1 --controlRegion SR3 --selectEstimator fakes-DD --year 2018 #SPLIT308
+python run_estimate.py --cores 1 --controlRegion SR4p --selectEstimator fakes-DD --year 2018 #SPLIT308
+
+#python run_estimate.py --cores 1 --controlRegion SR3M3 --selectEstimator fakes-DDMC --year 2018 #SPLIT501
+#python run_estimate.py --cores 1 --controlRegion SR4pM3 --selectEstimator fakes-DDMC --year 2018 #SPLIT501
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3dRUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT335
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3pdRUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT53
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR4pdRUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT335
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3AbsEtaUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT334
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3pAbsEtaUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT44
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR4pAbsEtaUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT334
+#python run_estimate.py --cores 1 --controlRegion SR3PtUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT308
+#python run_estimate.py --noInclusive --cores 1 --controlRegion SR3pPtUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT97
+#python run_estimate.py --cores 1 --controlRegion SR4pPtUnfold --selectEstimator fakes-DDMC --year 2018 #SPLIT308
+#python run_estimate.py --cores 1 --controlRegion SR3 --selectEstimator fakes-DDMC --year 2018 #SPLIT308
+#python run_estimate.py --cores 1 --controlRegion SR4p --selectEstimator fakes-DDMC --year 2018 #SPLIT308
